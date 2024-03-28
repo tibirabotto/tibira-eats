11c184b1306cdc6e3dfe07df005c29adc8ca2560
Tibira Botto <tibira.botto@gmail.com>
2024-03-28 16:49:45 -0400

    .\.git\

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Category from "./components/Category";
+import Food from "./components/Food";
+import HeadlineCards from "./components/HeadlineCards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <HeadlineCards />
+      <Food/>
+      <Category/>
     </div>
   );
 }
